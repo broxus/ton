6e1f90b6ddf402a6b02d88dea5e20d344b6dc9c5
Ivan Kalinin <i.kalinin@dexpa.io>
2020-10-08 19:38:49 +0300

    Add params unpacking for the most part of config

--- tonlib/tonlib/LiteServerUtils.cpp
+++ tonlib/tonlib/LiteServerUtils.cpp
@@ -285,37 +285,236 @@ auto to_tonlib_api(const block::ValidatorSet& vset) -> tonlib_api_ptr<tonlib_api
                                                                       vset.main, vset.total_weight, std::move(list));
 }
 
+auto parse_config_addr(td::Ref<vm::Cell>&& cell) -> td::Result<tonlib_api_ptr<tonlib_api::liteServer_accountId>> {
+  if (cell.is_null() || vm::load_cell_slice(cell).size_ext() != 0x100) {
+    return td::Status::Error("failed to parse address from config");
+  }
+
+  ton::StdSmcAddress addr;
+  CHECK(vm::load_cell_slice(cell).prefetch_bits_to(addr));
+  return tonlib_api::make_object<tonlib_api::liteServer_accountId>(ton::masterchainId, addr.as_slice().str());
+}
+
+auto parse_config_vset(td::Ref<vm::Cell>&& cell) -> td::Result<tonlib_api_ptr<tonlib_api::liteServer_validatorSet>> {
+  TRY_RESULT(vset, block::ConfigInfo::unpack_validator_set(cell))
+  return to_tonlib_api(*vset);
+}
+
+using ConfigParam = block::gen::ConfigParam;
+
+auto parse_mint_price(td::Ref<vm::Cell>&& cell) -> td::Result<tonlib_api_ptr<tonlib_api::liteServer_configMintPrice>> {
+  ConfigParam::Record_cons6 mint_price;
+  if (cell.is_null() || !tlb::type_unpack_cell(cell, ConfigParam{6}, mint_price)) {
+    return td::Status::Error("failed to unpack mint price");
+  }
+  TRY_RESULT(mint_new_price, parse_grams(mint_price.mint_new_price))
+  TRY_RESULT(mint_add_price, parse_grams(mint_price.mint_add_price))
+  return tonlib_api::make_object<tonlib_api::liteServer_configMintPrice>(mint_new_price, mint_add_price);
+}
+
+auto parse_to_mint(td::Ref<vm::Cell>&& cell) -> td::Result<tonlib_api_ptr<tonlib_api::liteServer_configToMint>> {
+  block::gen::ExtraCurrencyCollection::Record cc;
+  ConfigParam::Record_cons7 to_mint_value;
+  if (cell.is_null() || !tlb::type_unpack_cell(cell, ConfigParam{7}, to_mint_value) ||
+      !tlb::csr_unpack(to_mint_value.to_mint, cc)) {
+    return td::Status::Error("failed to unpack to_mint");
+  }
+
+  std::vector<tonlib_api_ptr<tonlib_api::liteServer_currencyCollectionItem>> items;
+  vm::Dictionary currencies{cc.dict, 32};
+  block::gen::VarUInteger::Record value;
+  for (const auto& item : currencies) {
+    if (!tlb::csr_type_unpack(item.second, block::gen::t_VarUInteger_32, value)) {
+      return td::Status::Error("failed to unpack currency value");
+    }
+    TRY_RESULT(currency_value, to_tonlib_api(value.value))
+    items.emplace_back(
+        tonlib_api::make_object<tonlib_api::liteServer_currencyCollectionItem>(item.first.get_int(32), currency_value));
+  }
+
+  return tonlib_api::make_object<tonlib_api::liteServer_configToMint>(std::move(items));
+}
+
+auto parse_global_version(td::Ref<vm::Cell>&& cell)
+    -> td::Result<tonlib_api_ptr<tonlib_api::liteServer_configGlobalVersion>> {
+  block::gen::GlobalVersion::Record global_version;
+  ConfigParam::Record_cons8 global_version_value;
+  if (cell.is_null() || !tlb::type_unpack_cell(cell, ConfigParam{8}, global_version_value) ||
+      !tlb::csr_unpack(global_version_value.x, global_version)) {
+    return td::Status::Error("failed to unpack global version");
+  }
+  return tonlib_api::make_object<tonlib_api::liteServer_configGlobalVersion>(  //
+      global_version.version, global_version.capabilities);
+}
+
+auto parse_integer_array(td::Ref<vm::Cell>&& cell) -> td::Result<tonlib_api_ptr<tonlib_api::liteServer_configParams>> {
+  if (cell.is_null()) {
+    return td::Status::Error("failed to unpack config params");
+  }
+
+  std::vector<td::int32> result;
+  vm::Dictionary mandatory_params{cell, 32};
+  for (const auto& item : mandatory_params) {
+    result.emplace_back(item.first.get_int(32));
+  }
+  return tonlib_api::make_object<tonlib_api::liteServer_configParams>(std::move(result));
+}
+
+auto parse_config_proposal_setup(td::Ref<vm::Cell>&& cell)
+    -> td::Result<tonlib_api_ptr<tonlib_api::liteServer_configProposalSetup>> {
+  block::gen::ConfigProposalSetup::Record proposal_setup;
+  if (cell.is_null() || !tlb::unpack_cell(std::move(cell), proposal_setup)) {
+    return td::Status::Error("failed to unpack config proposal setup");
+  }
+  return tonlib_api::make_object<tonlib_api::liteServer_configProposalSetup>(
+      proposal_setup.min_tot_rounds, proposal_setup.max_tot_rounds, proposal_setup.min_wins, proposal_setup.max_losses,
+      proposal_setup.min_store_sec, proposal_setup.max_store_sec, proposal_setup.bit_price, proposal_setup.cell_price);
+}
+
+auto parse_config_voting_setup(td::Ref<vm::Cell>&& cell)
+    -> td::Result<tonlib_api_ptr<tonlib_api::liteServer_configVotingSetup>> {
+  block::gen::ConfigVotingSetup::Record config_voting_setup;
+  ConfigParam::Record_cons11 config_voting_setup_value;
+  if (cell.is_null() || !tlb::type_unpack_cell(cell, ConfigParam{11}, config_voting_setup_value) ||
+      !tlb::csr_unpack(config_voting_setup_value.x, config_voting_setup)) {
+    return td::Status::Error("failed to unpack voting setup");
+  }
+  TRY_RESULT(normal_params, parse_config_proposal_setup(std::move(config_voting_setup.normal_params)))
+  TRY_RESULT(critical_params, parse_config_proposal_setup(std::move(config_voting_setup.critical_params)))
+  return tonlib_api::make_object<tonlib_api::liteServer_configVotingSetup>(  //
+      std::move(normal_params), std::move(critical_params));
+}
+
+auto parse_workchain_descr(td::Ref<vm::CellSlice>&& cs)
+    -> td::Result<tonlib_api_ptr<tonlib_api::liteServer_configWorkchainInfo>> {
+  block::gen::WorkchainDescr::Record info;
+  if (cs.is_null() || !tlb::csr_unpack(cs, info)) {
+    return td::Status::Error("failed to unpack workchain description");
+  }
+
+  block::gen::WorkchainFormat::Record_wfmt_basic workchain_format;
+  if (!tlb::csr_type_unpack(info.format, block::gen::WorkchainFormat{block::gen::WorkchainFormat::wfmt_basic},
+                            workchain_format)) {
+    return td::Status::Error("failed to unpack workchain format");
+  }
+
+  auto format = tonlib_api::make_object<tonlib_api::liteServer_configWorkchainFormat>(  //
+      workchain_format.vm_version, workchain_format.vm_mode);
+
+  return tonlib_api::make_object<tonlib_api::liteServer_configWorkchainInfo>(
+      info.enabled_since, info.actual_min_split, info.min_split, info.max_split, info.basic, info.active,
+      info.accept_msgs, info.flags, info.zerostate_root_hash.as_slice().str(),
+      info.zerostate_file_hash.as_slice().str(), info.version, std::move(format));
+}
+
+auto parse_config_workchains(td::Ref<vm::Cell>&& cell)
+    -> td::Result<tonlib_api_ptr<tonlib_api::liteServer_configWorkchains>> {
+  ConfigParam::Record_cons12 workchains_value;
+  if (cell.is_null() || !tlb::type_unpack_cell(cell, ConfigParam{12}, workchains_value)) {
+    return td::Status::Error("failed to unpack config workchains");
+  }
+
+  std::vector<tonlib_api_ptr<tonlib_api::liteServer_configWorkchainInfo>> result{};
+  vm::Dictionary workchains{workchains_value.workchains, 32};
+  for (const auto& item : workchains) {
+    auto value = item.second;
+    TRY_RESULT(workchain_info, parse_workchain_descr(std::move(value)))
+    result.emplace_back(std::move(workchain_info));
+  }
+  return tonlib_api::make_object<tonlib_api::liteServer_configWorkchains>(std::move(result));
+}
+
+template <typename T>
+auto parse_config_param(block::Config& config, int param, td::Result<tonlib_api_ptr<T>> (*f)(td::Ref<vm::Cell>&&))
+    -> td::Result<tonlib_api_ptr<T>> {
+  tonlib_api_ptr<T> result{};
+  if (auto param_ref = config.get_config_param(param); param_ref.not_null()) {
+    TRY_RESULT_ASSIGN(result, (*f)(std::move(param_ref)))
+  }
+  return result;
+}
+
 auto parse_config(const ton::BlockIdExt& blkid, td::Slice state_proof, td::Slice config_proof)
     -> td::Result<tonlib_api_ptr<tonlib_api::liteServer_configInfo>> {
+  enum {
+    CONFIG_ADDR = 0,
+    ELECTOR_ADDR = 1,
+    MINTER_ADDR = 2,
+    FEE_COLLECTOR_ADDR = 3,
+    DNS_ROOT_ADDR = 4,
+
+    MINT_PRICE = 6,
+    TO_MINT = 7,
+    GLOBAL_VERSION = 8,
+    MANDATORY_PARAMS = 9,
+    CRITICAL_PARAMS = 10,
+    CONFIG_VOTING_SETUP = 11,
+    WORKCHAINS = 12,
+    COMPLAINT_PRICING = 13,
+    BLOCK_CREATE_FEES = 14,
+    VALIDATORS_TIMINGS = 15,
+    VALIDATORS_QUANTITY_LIMITS = 16,
+    VALIDATORS_STAKE_LIMITS = 17,
+    STORAGE_PRICES = 18,
+
+    MASTERCHAIN_GAS_PRICES = 20,
+    GAS_PRICES = 21,
+    MASTERCHAIN_BLOCK_LIMITS = 22,
+    BLOCK_LIMITS = 23,
+    MASTERCHAIN_MSG_FORWARD_PRICES = 24,
+    MSG_FORWARD_PRICES = 25,
+
+    CATCHAIN_CONFIG = 28,
+    CONSENSUS_CONFIG = 29,
+
+    FUNDAMENTAL_SMC_ADDR = 31,
+
+    PREV_VSET = 32,
+    PREV_TEMP_VSET = 33,
+    CURR_VSET = 34,
+    CURR_TEMP_VSET = 35,
+    NEXT_VSET = 36,
+    NEXT_TEMP_VSET = 37,
+
+    VALIDATOR_SIGNED_TEMP_KEY = 39,
+  };
+
   TRY_RESULT(state_proof_ref, vm::std_boc_deserialize(state_proof))
   TRY_RESULT(config_proof_ref, vm::std_boc_deserialize(config_proof))
   TRY_RESULT(state, block::check_extract_state_proof(blkid, state_proof, config_proof))
 
   TRY_RESULT(config, block::ConfigInfo::extract_from_state(state, block::ConfigInfo::needShardHashes))
 
-  enum { PREV_VSET = 32, CURR_VSET = 34, NEXT_VSET = 36 };
+  // Accounts
+  TRY_RESULT(config_addr, parse_config_param(*config, CONFIG_ADDR, parse_config_addr))
+  TRY_RESULT(elector_addr, parse_config_param(*config, ELECTOR_ADDR, parse_config_addr))
+  TRY_RESULT(minter_addr, parse_config_param(*config, MINTER_ADDR, parse_config_addr))
+  TRY_RESULT(fee_collector_addr, parse_config_param(*config, FEE_COLLECTOR_ADDR, parse_config_addr))
+  TRY_RESULT(dns_root_addr, parse_config_param(*config, DNS_ROOT_ADDR, parse_config_addr))
 
-  tonlib_api_ptr<tonlib_api::liteServer_validatorSet> prev_vset{};
-  tonlib_api_ptr<tonlib_api::liteServer_validatorSet> curr_vset{};
-  tonlib_api_ptr<tonlib_api::liteServer_validatorSet> next_vset{};
+  // General
+  TRY_RESULT(mint_price, parse_config_param(*config, MINT_PRICE, parse_mint_price))
+  TRY_RESULT(to_mint, parse_config_param(*config, TO_MINT, parse_to_mint))
+  TRY_RESULT(global_version, parse_config_param(*config, GLOBAL_VERSION, parse_global_version))
+  TRY_RESULT(mandatory_params, parse_config_param(*config, MANDATORY_PARAMS, parse_integer_array))
+  TRY_RESULT(critical_params, parse_config_param(*config, CRITICAL_PARAMS, parse_integer_array))
+  TRY_RESULT(config_voting_setup, parse_config_param(*config, CONFIG_VOTING_SETUP, parse_config_voting_setup))
+  TRY_RESULT(workchains, parse_config_param(*config, WORKCHAINS, parse_config_workchains))
 
-  if (auto param = config->get_config_param(PREV_VSET); param.not_null()) {
-    TRY_RESULT(vset, block::ConfigInfo::unpack_validator_set(param))
-    prev_vset = to_tonlib_api(*vset);
-  }
+  // Validators
+  TRY_RESULT(prev_vset, parse_config_param(*config, PREV_VSET, parse_config_vset))
+  TRY_RESULT(prev_temp_vset, parse_config_param(*config, PREV_TEMP_VSET, parse_config_vset))
+  TRY_RESULT(curr_vset, parse_config_param(*config, CURR_VSET, parse_config_vset))
+  TRY_RESULT(curr_temp_vset, parse_config_param(*config, CURR_TEMP_VSET, parse_config_vset))
+  TRY_RESULT(next_vset, parse_config_param(*config, NEXT_VSET, parse_config_vset))
+  TRY_RESULT(next_temp_vset, parse_config_param(*config, NEXT_TEMP_VSET, parse_config_vset))
 
-  if (auto param = config->get_config_param(CURR_VSET); param.not_null()) {
-    TRY_RESULT(vset, block::ConfigInfo::unpack_validator_set(param))
-    curr_vset = to_tonlib_api(*vset);
-  }
-
-  if (auto param = config->get_config_param(NEXT_VSET); param.not_null()) {
-    TRY_RESULT(vset, block::ConfigInfo::unpack_validator_set(param))
-    next_vset = to_tonlib_api(*vset);
-  }
-
-  return tonlib_api::make_object<tonlib_api::liteServer_configInfo>(to_tonlib_api(blkid), std::move(prev_vset),
-                                                                    std::move(curr_vset), std::move(next_vset));
+  return tonlib_api::make_object<tonlib_api::liteServer_configInfo>(
+      to_tonlib_api(blkid), std::move(config_addr), std::move(elector_addr), std::move(minter_addr),
+      std::move(fee_collector_addr), std::move(dns_root_addr), std::move(mint_price), std::move(to_mint),
+      std::move(global_version), std::move(mandatory_params), std::move(critical_params),
+      std::move(config_voting_setup), std::move(workchains), std::move(prev_vset), std::move(prev_temp_vset),
+      std::move(curr_vset), std::move(curr_temp_vset), std::move(next_vset), std::move(next_temp_vset));
 }
 
 }  // namespace tonlib
